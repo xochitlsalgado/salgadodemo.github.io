012f460c18223c3ae660f32c5457bf7e5128e86e
xochitlSalgado <seguraxochitl00@gmail.com>
2026-09-08 21:15:21 -0600

    actualizar vista y logica de certificados

--- salgado.github.io/src/app/certificates/certificates.component.ts
+++ salgado.github.io/src/app/certificates/certificates.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; // Incluye JsonPipe, NgIf, NgFor
 import { CertificatesService, Certificate } from '../services/certificates.service';
 
 @Component({
   selector: 'app-certificates',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './certificates.component.html', // <-- ASEGÚRATE DE QUE EL NOMBRE SEA EXACTO (ej. ./certificates.component.html o ./certificates.html)
+  templateUrl: './certificates.component.html', // O ./certificates.html según el nombre exacto de tu archivo
   styleUrls: []
 })
 export class CertificatesComponent implements OnInit {
-  certificates: Certificate[] = [];
+  certificates: any[] = [];
 
   constructor(private certService: CertificatesService) {}
 
   ngOnInit(): void {
-    this.certService.getCertificates().subscribe((data: Certificate[]) => {
+    this.certService.getCertificates().subscribe((data: any[]) => {
       this.certificates = data;
+      console.log('Datos recibidos de Firebase:', data);
     });
   }
 }
